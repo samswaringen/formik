b67de1f691a8260eaafbca3a4f746f22bac0f12e
samswaringen <76227866+samswaringen@users.noreply.github.com>
2021-04-19 22:08:40 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import ExtraForm from './components/formdemo';
+
+function App() {
+  return (
+    <div>
+      <ExtraForm /><ExtraForm />
+    </div>
+  );
+}
+
+export default App;
